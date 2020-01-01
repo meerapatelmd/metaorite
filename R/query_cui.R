c49862a9a8f6bc8062844d7f0ad1755e180f1826
Meera Patel <patelm9@mskcc.org>
2019-12-31 20:50:15 -0500

    +

--- R/query_cui.R
+++ R/query_cui.R
@@ -1,5 +1,5 @@
 #' Queries cui in UMLS in MRCONSO table of metathesaurus
-#' @param umls_cui C-prefixed CUI from metathesaurus
+#' @param cui C-prefixed CUI from metathesaurus
 #' @param limit LIMIT in the sql statement. If NULL, all records will be returned.
 #' @return resultset from SQL query
 #' @importFrom mySeagull get_query
@@ -10,10 +10,10 @@ query_cui <-
         function(cui, limit = NULL) {
 
                 if (is.null(limit)) {
-                        sql_statement <- paste0("SELECT * FROM MRCONSO WHERE CUI = '", umls_cui, "' AND
+                        sql_statement <- paste0("SELECT * FROM MRCONSO WHERE CUI = '", cui, "' AND
                                                                         LAT = 'ENG' AND ISPREF = 'Y';")
                 } else {
-                        sql_statement <- paste0("SELECT * FROM MRCONSO WHERE CUI = '", umls_cui, "' AND
+                        sql_statement <- paste0("SELECT * FROM MRCONSO WHERE CUI = '", cui, "' AND
                                                                         LAT = 'ENG' AND ISPREF = 'Y' LIMIT ", limit, ";")
                 }
 
