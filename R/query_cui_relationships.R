ad4c7a9731c56edd2ed5741788550a59979410be
Meera Patel <patelm9@mskcc.org>
2020-01-15 10:30:22 -0500

    +: functions and function mods from queries for neuconc

--- R/query_cui_relationships.R
+++ R/query_cui_relationships.R
@@ -7,6 +7,23 @@ query_cui_relationships <-
                 sql_statement <-
                         paste0("SELECT * FROM MRREL WHERE CUI1 = '", cui, "';")
 
-                output <- mySeagull::get_query("umls", sql_statement)
+                output_01 <- mySeagull::get_query("umls", sql_statement)
+
+                if (nrow(output) > 0) {
+                        sql_statement <-
+                                paste0("SELECT * FROM MRCONSO WHERE CUI IN (", paste(paste0("'",output$CUI2, "'"), collapse = ", "), ");"
+                                       )
+                        output_02 <- mySeagull::get_query("umls", sql_statement)
+                        output_02 <-
+                                output_02 %>%
+                                dplyr::rename_all(function(x) paste0("MRCONSO_", x))
+                }
+
+                output <- dplyr::left_join(output_01,
+                                           output_02,
+                                           by = c("CUI2" = "MRCONSO_CUI"),
+                                           suffix = c(".mrrel", ".mrconso"))
+
+                output <- split(output, output$REL)
                 return(output)
         }
