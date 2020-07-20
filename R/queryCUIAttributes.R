7bb7c2a428a7e858993bc727cc2a10bda5eeeeb9
Meera Patel <patelm9@mskcc.org>
2020-07-20 11:35:50 -0400

    bug fix queryCUIAttributes

---
+++ R/queryCUIAttributes.R
@@ -0,0 +1,22 @@
+#' Query for source definition
+#' @export
+
+queryCUIAttributes <-
+        function(cui) {
+                base <- system.file(package = "metaorite")
+                path <- paste0(base, "/sql/mrsatAttributes.sql")
+                sql_statement <- 
+                    SqlRender::render(SqlRender::readSql(path),
+                                      cui = cui)
+                
+                resultset <- loadCacheSQL(sql_statement = sql_statement)
+                
+                if (is.null(resultset)) {
+                    
+                        resultset <- submitQuery(sql_statement)
+                        cacheSQL(object = resultset,
+                                 sql_statement = sql_statement)
+                        
+                }
+                return(output)
+        }
