71e04bc00a848143819b6fc56671fbe3055554ae
Meera Patel <patelmeeray@gmail.com>
2020-08-30 14:00:37 -0400

    add getAbbreviationInfo function

--- R/nihSite.R
+++ R/nihSite.R
@@ -19,3 +19,54 @@ getVocabularyInfo <-
                         rvest::html_node("table") %>%
                         rvest::html_table()
         }
+
+
+#' @title
+#' Get Abbreviation Information
+#' @description
+#' Get all the tables that provide abbreviation information at the NIH site \href{https://www.nlm.nih.gov/research/umls/knowledge_sources/metathesaurus/release/abbreviations.html}{Abbreviations}
+#' @return
+#' A list of data frames scraped from \href{https://www.nlm.nih.gov/research/umls/knowledge_sources/metathesaurus/release/abbreviations.html}{Abbreviations}
+#' @seealso
+#'  \code{\link[xml2]{read_xml}}
+#'  \code{\link[rvest]{html_nodes}},\code{\link[rvest]{html_table}}
+#'  \code{\link[purrr]{set_names}}
+#' @rdname getAbbreviationInfo
+#' @export
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_nodes html_table
+#' @importFrom purrr set_names
+#' @importFrom magrittr %>%
+
+getAbbreviationInfo <-
+        function() {
+                url <- "https://www.nlm.nih.gov/research/umls/knowledge_sources/metathesaurus/release/abbreviations.html"
+                data <- xml2::read_html(url)
+
+                tableNames <-
+
+                        tryCatch(
+                                data %>%
+                                        rvest::html_nodes("div p") %>%
+                                        .[1] %>%
+                                        html_text() %>%
+                                        strsplit(split = "[|]{1}") %>%
+                                        unlist() %>%
+                                        trimws(),
+                                error = function(e) NULL)
+
+
+
+                payLoad <-
+                        data %>%
+                                rvest::html_nodes("table") %>%
+                                rvest::html_table(fill = TRUE)
+
+                if (length(payLoad) == length(tableNames)) {
+                        payLoad %>%
+                                purrr::set_names(tableNames)
+                } else {
+                        payLoad
+                }
+        }
+
