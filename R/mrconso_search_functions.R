f47ef987cdd4a8c3b46ae97a2287e516179b4dbe
Meera <patelmeeray@gmail.com>
2020-09-27 23:14:47 -0400

    update functions

--- R/mrconso_search_functions.R
+++ R/mrconso_search_functions.R
@@ -13,9 +13,10 @@
 #' @param override_cache If TRUE, the cache is not loaded, the database is directly queried and the resultset is cached, overwriting any existing cache that may have been present.
 #'
 #' @return
-#' Dataframe of 0 or more rows
+#' Dataframe of 0 or more rows of all the fields in the MRCONSO Table.
 #'
 #' @name mrconso_search_functions
+#' @family search mrconso
 NULL
 
 
@@ -29,6 +30,7 @@ NULL
 #'  \code{\link[SqlRender]{render}}
 #'  \code{\link[pg13]{query}}
 #' @rdname search_exact
+#' @family search mrconso
 #' @export
 #' @importFrom SqlRender render
 #' @importFrom pg13 query
@@ -98,7 +100,8 @@ search_exact <-
                 }
 
 
-                resultset
+                resultset %>%
+                        arrange_nchar()
 
         }
 
@@ -115,7 +118,8 @@ search_exact <-
 #' @seealso
 #'  \code{\link[SqlRender]{render}}
 #'  \code{\link[pg13]{query}}
-#' @rdname search_exact
+#' @rdname search_like
+#' @family search mrconso
 #' @export
 #' @importFrom SqlRender render
 #' @importFrom pg13 query
@@ -186,7 +190,8 @@ search_like <-
                 }
 
 
-                resultset
+                resultset %>%
+                        arrange_nchar()
 
         }
 
@@ -203,6 +208,7 @@ search_like <-
 #' @seealso
 #'  \code{\link[pg13]{buildQueryString}},\code{\link[pg13]{query}}
 #' @rdname search_string
+#' @family search mrconso
 #' @export
 #' @importFrom pg13 buildQueryString query
 
@@ -212,6 +218,7 @@ search_string <-
                  split = " ",
                  schema = "mth",
                  match_case = TRUE,
+                 cache_only = FALSE,
                  skip_cache = FALSE,
                  override_cache = FALSE) {
 
@@ -276,6 +283,116 @@ search_string <-
                 }
 
 
+                resultset %>%
+                        arrange_nchar()
+
+        }
+
+
+#' @title
+#' Search for Synonyms
+#'
+#' @description
+#' This function performs a combination of searches using the \code{\link{search_mrconso_functions}} on a `concept`. The resultsets are then rejoined back onto the MRCONSO to get all possible strings. The direction is therefore from the `concept` to the CUI it matches and then from the CUI back to all the STR values that belong to the CUI. The types of search to use depends on the `concept`. For example, a very long gene name may not have an exact match, but may have very high yield 'string' matches because of the number of unique words. A 'like' match may be the least ideal since it casts a wide net in the search and the return may be to wide in breadth to search the use case.
+#'
+#' @return
+#' The results of the search will have all the fields from the MRCONSO Table, + search_type
+#'
+
+
+search_synonym <-
+        function(conn,
+                 concept,
+                 schema = "mth",
+                 search_types = c("exact", "string", "like"),
+                 split = " ",
+                 match_case = TRUE,
+                 cache_only = FALSE,
+                 skip_cache = FALSE,
+                 override_cache = FALSE) {
+
+
+                resultset <- tibble::tibble()
+
+                if ("exact" %in% search_types) {
+
+                         resultset <-
+                                 dplyr::bind_rows(resultset,
+                                                        search_exact(conn = conn,
+                                                                     concept = concept,
+                                                                     schema = schema,
+                                                                     match_case = match_case,
+                                                                     cache_only = cache_only,
+                                                                     skip_cache = skip_cache,
+                                                                     override_cache = override_cache) %>%
+                                                        dplyr::mutate(search_type = "exact")
+                                 )
+
+                }
+
+                if ("like" %in% search_types) {
+
+                        resultset <-
+                                dplyr::bind_rows(resultset,
+                                                 search_like(conn = conn,
+                                                              concept = concept,
+                                                              schema = schema,
+                                                              match_case = match_case,
+                                                              cache_only = cache_only,
+                                                              skip_cache = skip_cache,
+                                                              override_cache = override_cache) %>%
+                                                         dplyr::mutate(search_type = "like")
+                                )
+
+                }
+
+
+                if ("string" %in% search_types) {
+
+                        resultset <-
+                                dplyr::bind_rows(resultset,
+                                                 search_string(conn = conn,
+                                                             concept = concept,
+                                                             split = split,
+                                                             schema = schema,
+                                                             match_case = match_case,
+                                                             cache_only = cache_only,
+                                                             skip_cache = skip_cache,
+                                                             override_cache = override_cache) %>%
+                                                         dplyr::mutate(search_type = "string")
+                                )
+
+                }
+
+
+                tempTableName <-
+                        write_temp_table(conn = conn,
+                                         schema = schema,
+                                         data = resultset %>%
+                                                 dplyr::select(resultset_cui = cui, search_type) %>%
+                                                 dplyr::distinct())
+
+                sql_statement <-
+                        SqlRender::render(
+                                "SELECT a.search_type, b.*
+                                FROM @schema.@tempTableName a
+                                LEFT JOIN mth.mrconso b
+                                ON a.resultset_cui = b.cui;",
+                                schema = schema,
+                                tempTableName = tempTableName)
+
+                resultset <- pg13::query(conn = conn,
+                                                sql_statement = sql_statement)
+
+                drop_temp_table(conn = conn,
+                                schema = schema)
+
                 resultset
 
         }
+
+
+
+
+
+
