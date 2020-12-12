7a95035f8f0cdb945d04abdc083f93fbf60e1f61
Meera Patel <patelmeeray@gmail.com>
2020-12-12 03:01:27 -0500

    deploy v1.0.0

---
+++ construction_zone/medallion.R
@@ -0,0 +1,13 @@
+library(easyBakeOven)
+easyBakeOven::activateGoogleFont("Russo One")
+createMedallion(sourceImg = "logo.jpg",
+                package = "metaorite",
+                p_family = "Russo One",
+                white_around_sticker = TRUE ,
+                 s_width = 1.2,
+                s_height = 1.2,
+                s_y = 1.2,
+                s_x = 1.2,
+                filename = "logo.png"
+                 )
+createFavicons()
