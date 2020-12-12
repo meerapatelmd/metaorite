93119b433e87c98a4c3ecfc6cfe12ed799f881f1
Meera Patel <patelmeeray@gmail.com>
2020-12-12 03:16:49 -0500

    combine utils files

--- R/utils.R
+++ R/utils.R
@@ -121,4 +121,15 @@ drop_temp_table <-
         }
 
 
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @noRd
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
 
